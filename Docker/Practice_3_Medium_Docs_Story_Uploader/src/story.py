47ae05d1120a88921029f7b3b1d738f06d65d1ad
yuval1847 <yuval1847@gmail.com>
2025-11-08 01:36:27 +0200

    Continue the MDSU project

---
+++ Docker/Practice_3_Medium_Docs_Story_Uploader/src/story.py
@@ -0,0 +1,35 @@
+class story:
+    """
+    A class which represents a story to be uploaded to Medium.com
+    """
+
+    def __init__(self, title="", content=[]):
+        self.title = title
+        self.content = content
+        
+
+    def add_text(self, paragraph_text:str):
+        """
+        Add a string to the story content.
+        """
+        self.content.append({"type": "text", "value": paragraph_text})
+
+
+    def add_image(self, image_path:str):
+        """
+        Add an image to the story content.
+        """
+        self.content.append({"type": "image", "value": image_path})
+
+
+    def get_content_as_html(self):
+        """
+        Get the story content as a single HTML string.
+        """
+        html_content = ""
+        for item in self.content:
+            if item["type"] == "text":
+                html_content += f'<p>{item['value']}</p>\n'
+            elif item["type"] == "image":
+                html_content += f'<img src="{item['value']}"\n'
+        return html_content
--- Docker/Practice_3_Medium_Docs_Story_Uploader/src/uploading_algo.py
+++ Docker/Practice_3_Medium_Docs_Story_Uploader/src/uploading_algo.py
@@ -1,7 +1,15 @@
 import json
 import requests
+import zipfile
+import os
+import time
+
 from docx import Document
+from lxml import etree
 from constants import *
+from story import *
+
+
 
 class medium_docs_upload_session:
     """
@@ -11,6 +19,7 @@ class medium_docs_upload_session:
     def __init__(self):
         self.user_integration_token = None
         self.user_id = None
+        self.new_story = story()
 
 
     def connect(self, user_integration_token:str):
@@ -30,9 +39,100 @@ class medium_docs_upload_session:
             self.user_id = medium_user_id_request.json()['data']['id']
     
 
-    def upload_docs_file(self, file_path:str):
+    def read_docs_file(self, docx_path:str):
         """
-        Read the content of a .docs file while uploading its content to the site medium as a new story.
+        Read the content of a .docs file and returns its content as a string in HTML format.
         """
-        doc = Document(file_path)
-        # Continue the uploading function...
+        # A temporary folder for images
+        tmp_dir = f"tmp_docx_images_{int(time.time())}"
+        os.makedirs(tmp_dir, exist_ok=True)
+
+        # Extract the document.xml file (the real content)
+        with zipfile.ZipFile(docx_path) as docx_zip:
+            xml_content = docx_zip.read("word/document.xml")
+
+            # Map image IDs to actual file names
+            rels_xml = docx_zip.read("word/_rels/document.xml.rels")
+            rels_tree = etree.fromstring(rels_xml)
+            rels = {}
+            for rel in rels_tree:
+                rid = rel.attrib.get("{http://schemas.openxmlformats.org/officeDocument/2006/relationships}id")
+                target = rel.attrib.get("{http://schemas.openxmlformats.org/officeDocument/2006/relationships}target")
+                if target.startswith("media/"):
+                    rels[rid] = target
+
+           # Parse document XML
+            tree = etree.fromstring(xml_content)
+            ns = {
+                "w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
+                "a": "http://schemas.openxmlformats.org/drawingml/2006/main",
+                "r": "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
+            }
+
+            image_counter = 1
+
+            # Iterate over document body elements
+            for element in tree.findall(".//w:body/*", ns):
+                # If paragraph
+                if element.tag == "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}p":
+                    text = "".join(node.text for node in element.findall(".//w:t", ns) if node.text)
+                    if text.strip():
+                        self.new_story.add_text(text)
+
+                # If image (drawing)
+                for blip in element.findall(".//a:blip", ns):
+                    rid = blip.attrib.get("{http://schemas.openxmlformats.org/officeDocument/2006/relationships}embed")
+                    if rid in rels:
+                        img_path = "word/" + rels[rid]
+                        img_data = docx_zip.read(img_path)
+
+                        # Give each image a separate filename
+                        img_name = f"image_{image_counter}" + os.path.splitext(rels[rid])[1]
+                        img_output_path = os.path.join(tmp_dir, img_name)
+
+                        # Save image file
+                        with open(img_output_path, "wb") as f:
+                            f.write(img_data)
+
+                        # Upload image to Medium
+                        image_url = self.upload_image(img_output_path)
+                        self.new_story.add_image(image_url)
+                        
+                        image_counter += 1
+
+
+    def upload_image(self, image):
+        """
+        Upload an image to Medium.com and return the image URL.
+        """
+        pass
+
+
+    def upload_story_content(self, story_title:str, story_content_html:str, publish_status:str="draft"):
+        """
+        Upload the story content to Medium.com
+        """
+        if self.user_id is None or self.user_integration_token is None:
+            raise Exception("Not connected to Medium API. Please call connect() first.")
+        
+        headers = {'Content-type': 'application/json',
+                   'Accept': 'text/plain',
+                   'Host': MEDIUM_API_SUBDOMAIN,
+                   'Accept-Charset': 'utf-8',
+                   "Authorization": self.user_integration_token}
+
+        post_data = {
+            "title": story_title,
+            "contentFormat": "html",
+            "content": story_content_html,
+            "publishStatus": publish_status
+        }
+
+        medium_story_upload_request = requests.post("https://"+MEDIUM_API_SUBDOMAIN+"/v1/users/"+self.user_id+"/posts", 
+                                                    headers=headers, 
+                                                    data=json.dumps(post_data))
+        
+        if medium_story_upload_request.status_code != 201:
+            raise Exception("Failed to upload story to Medium")
+        else:
+            return medium_story_upload_request.json()['data']['url']
